028a933b2889b09f2dfbd748c763c18ae5c2c857
Abhishek Sahu <rjdeveloper07@gmail.com>
2025-02-14 19:34:56 +0530

    14th

--- src/App.jsx
+++ src/App.jsx
@@ -42,115 +42,115 @@ const App=()=>{
 
 
 
-// bootstrap------------------------
+bootstrap------------------------
 
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
-
-
-
-
-// import Carousel from 'react-bootstrap/Carousel';
-// // import ExampleCarouselImage from 'components/ExampleCarouselImage';
-// import b1 from "./img/a1.jpg";
-// import b2 from "./img/a2.jpg";
-// import b3 from "./img/a3.jpg";
-
-
-// import Stack from 'react-bootstrap/Stack';
-
-// import Button from 'react-bootstrap/Button';
-// import Card from 'react-bootstrap/Card';
-
-
-// const App=()=>{
-//     return(
-//         <>
-//         <Navbar expand="lg" className="bg-body-tertiary">
-//       <Container>
-//         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//           <Nav className="me-auto">
-//             <Nav.Link href="#home">Home</Nav.Link>
-//             <Nav.Link href="#link">Link</Nav.Link>
-//             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-//               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-//               <NavDropdown.Item href="#action/3.2">
-//                 Another action
-//               </NavDropdown.Item>
-//               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-//               <NavDropdown.Divider />
-//               <NavDropdown.Item href="#action/3.4">
-//                 Separated link
-//               </NavDropdown.Item>
-//             </NavDropdown>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 
 
-
-//     <Carousel>
-//       <Carousel.Item>
-//         {/* <ExampleCarouselImage text="First slide" /> */}
-//         <img src={b1} style={{width:"100%", height:"300px"}}  alt="" />
-//         <Carousel.Caption>
-//           <h3>First slide label</h3>
-//           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-//         </Carousel.Caption>
-//       </Carousel.Item>
-//       <Carousel.Item>
-//       <img src={b2} style={{width:"100%", height:"300px"}} alt="" />
-//       <Carousel.Caption>
-//           <h3>Second slide label</h3>
-//           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-//         </Carousel.Caption>
-//       </Carousel.Item>
-//       <Carousel.Item>
-//       <img src={b3} style={{width:"100%", height:"300px"}} alt="" />
-//       <Carousel.Caption>
-//           <h3>Third slide label</h3>
-//           <p>
-//             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-//           </p>
-//         </Carousel.Caption>
-//       </Carousel.Item>
-//     </Carousel>
+import Carousel from 'react-bootstrap/Carousel';
+// import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import b1 from "./img/a1.jpg";
+import b2 from "./img/a2.jpg";
+import b3 from "./img/a3.jpg";
 
 
+import Stack from 'react-bootstrap/Stack';
 
-// {/* crd--------------------------------- */}
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-// <div style={{width:"100%", display:"flex"}}>
 
-//     <Card style={{ width: '18rem', display:"flex"}}>
-//       <img src={b3} />
-//       <Card.Body>
-//         <Card.Title>Mobile Shop Cart</Card.Title>
-//         <Card.Text>
-//             this is a mobile cart
-//         </Card.Text>
-//         <Button variant="primary">visite mobile </Button>
-//       </Card.Body>
-//     </Card>
+const App=()=>{
+    return(
+        <>
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 
-//     <Card style={{ width: '18rem' }}>
-//       {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-//       <img src={b3} />
-//       <Card.Body>
-//         <Card.Title>Mobile Shop Cart</Card.Title>
-//         <Card.Text>
-//             this is a mobile cart
-//         </Card.Text>
-//         <Button variant="primary">visite mobile </Button>
-//       </Card.Body>
-//     </Card>
+
+
+
+
+    <Carousel>
+      <Carousel.Item>
+        {/* <ExampleCarouselImage text="First slide" /> */}
+        <img src={b1} style={{width:"100%", height:"300px"}}  alt="" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={b2} style={{width:"100%", height:"300px"}} alt="" />
+      <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={b3} style={{width:"100%", height:"300px"}} alt="" />
+      <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+
+
+{/* crd--------------------------------- */}
+
+<div style={{width:"100%", display:"flex"}}>
+
+    <Card style={{ width: '18rem', display:"flex"}}>
+      <img src={b3} />
+      <Card.Body>
+        <Card.Title>Mobile Shop Cart</Card.Title>
+        <Card.Text>
+            this is a mobile cart
+        </Card.Text>
+        <Button variant="primary">visite mobile </Button>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem' }}>
+      {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+      <img src={b3} />
+      <Card.Body>
+        <Card.Title>Mobile Shop Cart</Card.Title>
+        <Card.Text>
+            this is a mobile cart
+        </Card.Text>
+        <Button variant="primary">visite mobile </Button>
+      </Card.Body>
+    </Card>
     
 // </div>
 
